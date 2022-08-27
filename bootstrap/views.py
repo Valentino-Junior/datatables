f68edd72e879933b954bf20307e891ed612d0842
valentine-ochieng <ojvalentine14@gmail.com>
2022-08-27 09:33:38 +0300

    added alert message with porgress bar

--- bootstrap/views.py
+++ bootstrap/views.py
@@ -19,27 +19,36 @@ def addnew(request):
     else:  
         form = EmployeeForm()  
 
-    messages.success(request, 'Order updated successfuly') 
+        
+
+
 
     return render(request,'index.html',{'form':form})  
 def index(request):  
     employees = Employee.objects.all()  
-    messages.success(request, 'Order updated successfuly') 
+    messages.success(request, 'Order created successfuly') 
+
 
     return render(request,"show.html",{'employees':employees})  
 def edit(request, id):  
     employee = Employee.objects.get(id=id)  
     return render(request,'edit.html', {'employee':employee})  
+
 def update(request, id):  
     employee = Employee.objects.get(id=id)  
     form = EmployeeForm(request.POST, instance = employee)  
     if form.is_valid():  
         form.save()  
-        return redirect("/")     
+        messages.success(request, 'Order updated successfuly') 
+        return redirect("/") 
+
     return render(request, 'edit.html', {'employee': employee})  
+
 def destroy(request, id):  
     employee = Employee.objects.get(id=id)  
     employee.delete()  
+    messages.success(request, 'Order deleted successfuly') 
+
     return redirect("/")  
 
 
