1c3458cdb4d30c1a926081ad5b4d0e5666376a6e
valentine-ochieng <ojvalentine14@gmail.com>
2022-12-20 22:44:14 +0300

    Add allowed host

--- datatables/settings.py
+++ datatables/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = 'django-insecure-8y-r=cl_r=vx94q$f9a*w3p30jekfl%x=eydfpqej@-6jbz$@w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["datatables-production.up.railway.app","datatables-production.up.railway.app/admin", "127.0.0.1"]
 
+CSRF_TRUSTED_ORIGINS = ['datatables-production.up.railway.app']
 
 # Application definition
 
