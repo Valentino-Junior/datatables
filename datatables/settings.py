26d66efdc9472e09b54ef7f952480a4149a2519b
valentine-ochieng <ojvalentine14@gmail.com>
2022-12-20 22:53:45 +0300

    Updated allowed host

--- datatables/settings.py
+++ datatables/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["datatables-production.up.railway.app","datatables-production.up.railway.app/admin", "127.0.0.1"]
 
-CSRF_TRUSTED_ORIGINS = ['datatables-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://datatables-production.up.railway.app/']
 
 # Application definition
 
