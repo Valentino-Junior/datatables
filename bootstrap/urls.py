72a2c03bc271fbc662f4c7fb9645eaa8a01eb9be
valentine-ochieng <ojvalentine14@gmail.com>
2022-12-29 17:48:49 +0300

    Add static files

--- bootstrap/urls.py
+++ bootstrap/urls.py
@@ -1,6 +1,9 @@
 #urls.py
 from django.urls import path  
 from .import views  
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [  
     path('', views.index),  
     path('addnew',views.addnew),  
@@ -22,3 +25,6 @@ urlpatterns = [
 
 
 ]  
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
