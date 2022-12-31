d67e2789efce5020d3481f25e78ef07b242775df
valentine-ochieng <ojvalentine14@gmail.com>
2022-12-31 03:39:19 +0300

    Add migration

--- bootstrap/models.py
+++ bootstrap/models.py
@@ -12,7 +12,7 @@ class Employee(models.Model):
 
 class Photo(models.Model):
     files = models.FileField(blank=True, null=True, upload_to='files/')
-    instructions = models.CharField(max_length=255, blank=True)
+    instructions = models.TextField(max_length=255, blank=True)
 
     def __str__(self):
         return self.instructions
